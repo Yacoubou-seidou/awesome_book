43a1efe239b03b7f0769339d5b33f0f66a35ab1e
yacoubou-seidou <yacoseidou@gmail.com>
2023-03-20 16:00:14 +0100

    Complete Javascript code

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const bookName = document.querySelector('#book-name');
 const bookAuthor = document.querySelector('#book-author');
 const addButton = document.querySelector('#add_book');
-const booksContainer = document.querySelector('#books_section');
+let booksContainer = document.querySelector('#books_section');
 
-let book_list = []
+let book_list = [];
+let localData = localStorage.getItem('books');
+if (localData) {
+  book_list = JSON.parse(localData);
+  console.log(book_list);
+}
 
 addButton.addEventListener('click', () => {
   const bookName_input = bookName.value;
   const bookAuthor_input = bookAuthor.value;
   addBook(bookName_input, bookAuthor_input);
+  bookAuthor.value = '';
+  bookName.value = '';
 });
 
-function addBook (bookName, bookAuthor) {
+function addBook(bookName, bookAuthor) {
   const book = {};
   book.title = bookName;
   book.author = bookAuthor;
   book_list.push(book);
-
-  addBookToPage(bookName, bookAuthor);
+  localStorage.setItem('books', JSON.stringify(book_list));
+  addBookToPage(bookName, bookAuthor, book_list.length - 1);
 }
 
-function addBookToPage (bookName, bookAuthor) {
+function addBookToPage(bookName, bookAuthor, id) {
   const bookContainer = document.createElement('div');
   bookContainer.classList.add('book_container');
+  bookContainer.classList.add(`book${id}`);
   const bookDetails = document.createElement('p');
   const bookName_input = bookName;
   const bookAuthor_input = bookAuthor;
@@ -32,16 +40,21 @@ function addBookToPage (bookName, bookAuthor) {
   const removeButton = document.createElement('button');
   removeButton.innerHTML = 'Remove';
   const breakLine = document.createElement('hr');
-  bookContainer.appendChild(bookDetails);  
+  bookContainer.appendChild(bookDetails);
   bookContainer.appendChild(removeButton);
   bookContainer.appendChild(breakLine);
   booksContainer.appendChild(bookContainer);
+  removeButton.addEventListener('click', () => {
+    book_list.splice(id, 1);
+    localStorage.setItem('books', JSON.stringify(book_list));
+    // location.reload();
+    booksContainer.removeChild(bookContainer);
+  });
 }
 
 function loadBooks(book_list) {
   for (let i = 0; i < book_list.length; i += 1) {
-    addBookToPage(book_list[i].name, book_list[i].author);
+    addBookToPage(book_list[i].title, book_list[i].author, i);
   }
 }
-
-window.onload = loadBooks;
+loadBooks(book_list);
