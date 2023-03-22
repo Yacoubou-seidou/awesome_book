f1c0bc9f1468ef468ef6dcbea91a967872752eae
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-22 10:17:32 +0300

    Refactor javascript to use modules

---
+++ book-list.js
@@ -0,0 +1,57 @@
+class BookList {
+  bookList = [];
+
+  constructor() {
+    this.bookNameInput = document.querySelector('#book-name');
+    this.bookAuthorInput = document.querySelector('#book-author');
+    this.addButton = document.querySelector('#add_book');
+    this.booksContainer = document.querySelector('#books_section');
+    this.bookForm = document.querySelector('.book-form');
+    this.localData = localStorage.getItem('books');
+
+    if (this.localData) {
+      this.bookList = JSON.parse(this.localData);
+    }
+
+    this.addButton.addEventListener('click', () => {
+      this.addBook(this.bookNameInput.value, this.bookAuthorInput.value);
+      this.bookForm.reset();
+    });
+  }
+
+  addBookToPage(bookName, bookAuthor, id) {
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book_container');
+    bookContainer.classList.add(`book${id}`);
+    if (id % 2 === 0) {
+      bookContainer.classList.add('dark-bg');
+    }
+    bookContainer.classList.add('book-item');
+    const bookDetails = document.createElement('p');
+    bookDetails.innerHTML = `
+      "${bookName}" by ${bookAuthor}
+    `;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    bookContainer.appendChild(bookDetails);
+    bookContainer.appendChild(removeButton);
+    this.booksContainer.appendChild(bookContainer);
+    removeButton.addEventListener('click', () => {
+      this.bookList.splice(id, 1);
+      localStorage.setItem('books', JSON.stringify(this.bookList));
+      this.booksContainer.removeChild(bookContainer);
+    });
+  }
+
+  // Add book to booklist, update local storage and page view
+  addBook(bookName, bookAuthor) {
+    const book = {};
+    book.title = bookName;
+    book.author = bookAuthor;
+    this.bookList.push(book);
+    localStorage.setItem('books', JSON.stringify(this.bookList));
+    this.addBookToPage(bookName, bookAuthor, this.bookList.length - 1);
+  }
+}
+
+export default BookList;
