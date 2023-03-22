c92b55a4634bb1330c47f8213c915e82710c93c5
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-22 16:53:21 +0300

    Add single page navigation

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 import BookList from './book-list.js';
 
 const bookObj = new BookList();
+const formContainer = document.querySelector('.book-form');
+const bookListSection = document.querySelector('.book-list-section');
+const contactSection = document.querySelector('.contact-section');
 
+const listLink = document.querySelector('#list-btn');
+const AddNewLink = document.querySelector('#add-btn');
+const contactLink = document.querySelector('#contact-btn');
+
+listLink.addEventListener('click', () => {
+  bookListSection.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+
+AddNewLink.addEventListener('click', () => {
+  bookListSection.style.display = 'none';
+  formContainer.style.display = 'flex';
+  contactSection.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  bookListSection.style.display = 'none';
+  contactSection.style.display = 'block';
+  formContainer.style.display = 'none';
+});
+
+const dateContainer = document.querySelector('.date-container');
+dateContainer.innerHTML = new Date();
 bookObj.loadBooks();
